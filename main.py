15c0c9169ab581e9f8cb50711c3c199299c35814
scanx001 <Sarp95can@gmail.com>
2019-11-21 17:27:22 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+def moveUp():
+    zeroIndex = ziel[1].index(0)
+    print(zeroIndex)
+
+zustandsstruktur = [
+    [None,None,None],
+    [None,None,None],
+    [None,None,None]
+    ]
+
+initialzustand = [
+    [2,8,3],
+    [1,6,4],
+    [7,0,5]
+]
+
+ziel = [
+    [1,2,3],
+    [8,0,4],
+    [7,6,5]
+]
+
+moveUp()
+
+
+
+
+
